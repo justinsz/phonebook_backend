3c1d558dcf345acfb07d57607f619b0178c01aa6
Justin "Fledd" Szczepaniak <justin.szczepaniak@rub.de>
2025-03-04 14:37:27 +0100

    better error messages

--- main.js
+++ main.js
@@ -154,6 +154,7 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
+    // Format validation errors to be more readable
     return response.status(400).json({ error: error.message })
   } else if (error.name === 'MongoServerError' && error.code === 11000) {
     return response.status(400).json({ error: 'name must be unique' })
