4a9f011e9e0690b7150d24e9aea5387ccb5ba179
Justin "Fledd" Szczepaniak <justin.szczepaniak@rub.de>
2025-03-04 13:59:39 +0100

    fix

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
+const Person = require('./models/person')
+
 const app = express()
 
 // Middleware
@@ -9,38 +12,34 @@ app.use(express.json())
 app.use(express.static('dist')) // Serve static files from dist folder
 app.use(morgan('tiny'))
 
-// Get data from a file for now
-let persons = [
-  { name: 'Arto Hellas', number: '040-123456', id: '1' },
-  { name: 'Ada Lovelace', number: '39-44-5323523', id: '2' },
-  { name: 'Dan Abramov', number: '12-43-234345', id: '3' },
-  { name: 'Mary Poppendieck', number: '39-23-6423122', id: '4' }
-]
-
 // Routes
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = req.params.id
-  const person = persons.find(person => person.id === id)
-  
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = req.params.id
-  persons = persons.filter(person => person.id !== id)
-  
-  res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const body = req.body
   
   if (!body.name || !body.number) {
@@ -49,54 +48,42 @@ app.post('/api/persons', (req, res) => {
     })
   }
   
-  if (persons.find(person => person.name === body.name)) {
-    return res.status(400).json({ 
-      error: 'name must be unique' 
-    })
-  }
-  
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: Math.random().toString(36).substr(2, 9)
-  }
+  })
   
-  persons = persons.concat(person)
-  
-  res.json(person)
+  person.save()
+    .then(savedPerson => {
+      res.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (req, res) => {
-  const id = req.params.id
-  const body = req.body
+app.put('/api/persons/:id', (req, res, next) => {
+  const { name, number } = req.body
   
-  if (!body.name || !body.number) {
-    return res.status(400).json({ 
-      error: 'name or number missing' 
+  Person.findByIdAndUpdate(
+    req.params.id, 
+    { name, number }, 
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedPerson => {
+      res.json(updatedPerson)
     })
-  }
-  
-  const person = {
-    name: body.name,
-    number: body.number,
-    id: id
-  }
-  
-  const index = persons.findIndex(p => p.id === id)
-  if (index !== -1) {
-    persons[index] = person
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+    .catch(error => next(error))
 })
 
 // Info page
-app.get('/info', (req, res) => {
-  res.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>
-  `)
+app.get('/info', (req, res, next) => {
+  Person.countDocuments({})
+    .then(count => {
+      res.send(`
+        <p>Phonebook has info for ${count} people</p>
+        <p>${new Date()}</p>
+      `)
+    })
+    .catch(error => next(error))
 })
 
 // For Render deployment - serve the frontend for any other routes
@@ -104,8 +91,23 @@ app.get('*', (req, res) => {
   res.sendFile('dist/index.html', { root: __dirname })
 })
 
+// Error handling middleware
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
 // Define PORT
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ models/person.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+// Make sure this URL points to the phonebookApp database
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+    // Log the database and collections for debugging
+    mongoose.connection.db.listCollections().toArray()
+      .then(collections => {
+        console.log('Collections:', collections.map(c => c.name))
+      })
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true
+  }
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+// This should create/use the 'people' collection
+module.exports = mongoose.model('Person', personSchema) 
